52a83169183b4cd5a944d6c51d5594058a122e67
110598067 <jack1996yuc@gmail.com>
2022-04-17 22:57:27 +0800

    try

---
+++ src/test/java/com/github/davidmoten/geo/mem/GeomemTest.java
@@ -0,0 +1,24 @@
+package com.github.davidmoten.geo.mem;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GeomemTest {
+    @Test
+    public void addWith4ParametersT1(){
+        Geomem<Integer, String> geomem = new Geomem<Integer, String>();
+        geomem.add(25.5,122,1,1);
+    }
+    @Test(expected = IllegalArgumentException.class)
+    public void addWith4ParametersT2(){
+        Geomem<Integer, String> geomem = new Geomem<Integer, String>();
+        geomem.add(-91,122,1,1);
+    }
+    @Test
+    public void addWith4ParametersT3(){
+        Geomem<Integer, String> geomem = new Geomem<Integer, String>();
+        geomem.add(60,188,1,1);
+    }
+
+}
